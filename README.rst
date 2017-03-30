e8674ecbdc14b86fe0f957ead2d881e5753d48f3
Hong Minhee <hong.minhee@gmail.com>
2017-03-30 16:23:42 +0900

    Version bump

--- README.rst
+++ README.rst
@@ -126,6 +126,12 @@ __ https://hongminhee.org/
 Changelog
 ---------
 
+Version 0.3.4
+`````````````
+
+To be released.
+
+
 Version 0.3.3
 `````````````
 
--- setup.py
+++ setup.py
@@ -57,8 +57,7 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
-        'License :: OSI Approved '
-        ':: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',  # noqa: E501
         'Operating System :: POSIX',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
@@ -68,9 +67,10 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: System :: Systems Administration :: Authentication/Directory',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',  # noqa: E501
         'Topic :: Utilities'
     ]
 )
