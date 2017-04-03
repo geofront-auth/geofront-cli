e7aa3f9f71c8e5e0b2cede33057a93b8924bf20c
Hong Minhee <hong.minhee@gmail.com>
2017-04-03 19:27:41 +0900

    More unified test

--- tests/client_test.py
+++ tests/client_test.py
@@ -3,5 +3,5 @@ from geofrontcli.client import parse_mimetype
 
 def test_parse_mimetype():
     assert parse_mimetype('text/plain') == ('text/plain', [])
-    assert parse_mimetype('text/html; charset=utf-8') == \
-           ('text/html', ['charset=utf-8'])
+    assert (parse_mimetype('text/html; charset=utf-8') ==
+            ('text/html', ['charset=utf-8']))
