ba39d315437c322e4fd5c640ceabf5b4879fb8cb
Kang Hyojun <iam.kanghyojun@gmail.com>
2020-03-25 10:12:53 +0900

    Release 0.4.3

--- geofrontcli/version.py
+++ geofrontcli/version.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 
 
 #: (:class:`tuple`) The triple of version numbers e.g. ``(1, 2, 3)``.
-VERSION_INFO = (0, 4, 2)
+VERSION_INFO = (0, 4, 3)
 
 #: (:class:`str`) The version string e.g. ``'1.2.3'``.
 VERSION = '{0}.{1}.{2}'.format(*VERSION_INFO)
